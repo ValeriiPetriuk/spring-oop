e09a90ac3df993bb4a8996f48ca977411a6190c1
ValeriiPetriuk <vpetruk257@gmail.com>
2024-12-09 10:16:02 +0200

    init

---
+++ src/test/java/com/oop/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oop.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
