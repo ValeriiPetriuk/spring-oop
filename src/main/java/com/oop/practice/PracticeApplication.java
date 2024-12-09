654666578c7d9fc1e01428aee43478e1ef54f399
ValeriiPetriuk <vpetruk257@gmail.com>
2024-12-09 11:52:35 +0200

    add essence inspection

--- src/main/java/com/oop/practice/PracticeApplication.java
+++ src/main/java/com/oop/practice/PracticeApplication.java
@@ -1,13 +1,22 @@
 package com.oop.practice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner CommandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("hello world");
+		};
 	}
 
 }
